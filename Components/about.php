8774798ea548cddac3c1c39e3d677da97b1181e2
ANIL MN <amn.nmb@gmail.com>
2020-04-19 16:14:54 +0530

    Updated

---
+++ Components/about.php
@@ -0,0 +1,71 @@
+<?php
+
+$about = '<!-- About Page 2 Area Start Here -->
+<div class="about-page2-area">
+    <div class="container">
+        <div class="row about-page2-inner">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <h2 class="title-default-left">About Our Institution</h2>
+                <p>Tmply dummy text of the printing and typesetting indust Lorem Ipsum has been theitry s snce simply dummy text of the printing.Phasellus enim libero, blandit vel sapien vitae, condimentum ultricies magna euismod.Tmply dummy text of the printing and typesetting indust Lorem Ipsum has been theitr.</p>
+                <ul>
+                    <li><a href="#">Materiality & Interpretation</a></li>
+                    <li><a href="#">Design Management and Cultural Enterprise</a></li>
+                    <li><a href="#">Experience Design (XD)</a></li>
+                    <li><a href="#">Sound Design; Social Media and SEO</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <div class="video-area2 overlay-video bg-common-style video-margin-top" style="background-image: url("img/banner/6.jpg");">
+                    <div class="video-content">
+                        <a class="play-btn popup-youtube" href="http://www.youtube.com/watch?v=1iIZeIy7TqM"><i class="fa fa-play" aria-hidden="true"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- About Page 2 Area End Here -->';
+
+
+$whyThis = '<!-- Why Choose Area Start Here -->
+<div class="why-choose-area">
+    <div class="container">
+        <h2 class="about-title">Why Choose Our Institution?</h2>
+        <p class="about-sub-title">Tmply dummy text of the printing and typesetting industry. Lorem Ipsum has been theindustrys standard dummy text ever since the 1500s, when an unknown printer took.</p>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="service-box4">
+                    <div class="service-box-icon">
+                        <a href="#"><i class="fa fa-graduation-cap" aria-hidden="true"></i></a>
+                    </div>
+                    <h3><a href="#">Scholarship Facility</a></h3>
+                    <p>Dorem Ipsum has been the industrys standard dummy text ever since the en an unknown printer galley dear.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="service-box4">
+                    <div class="service-box-icon">
+                        <a href="#"><i class="fa fa-user" aria-hidden="true"></i></a>
+                    </div>
+                    <h3><a href="#">Skilled Lecturers</a></h3>
+                    <p>Dorem Ipsum has been the industrys standard dummy text ever since the en an unknown printer galley dear.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                <div class="service-box4">
+                    <div class="service-box-icon">
+                        <a href="#"><i class="fa fa-book" aria-hidden="true"></i></a>
+                    </div>
+                    <h3><a href="#">Book Library & Store</a></h3>
+                    <p>Dorem Ipsum has been the industrys standard dummy text ever since the en an unknown printer galley dear.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Why Choose Area End Here -->';
+
+
+?>
---
+++ pages/firstPage.php
@@ -0,0 +1,11 @@
+<?php
+include('Components/head.php');
+include('Components/header.php');
+include('components/imageSlider.php');
+include('components/about.php');
+
+$firstPage = $Slider.$about.$whyThis;
+
+
+
+?>
