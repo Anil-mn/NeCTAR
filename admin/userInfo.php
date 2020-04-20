8eaa0cb57584253385316574879e0a1b7fd451f0
AmithaSuresh <amithasuresh097@gmail.com>
2020-04-20 22:36:08 +0530

    userinfo page added

---
+++ admin/userInfo.php
@@ -0,0 +1,10 @@
+<html>
+
+<?php
+include('components/head.php');
+include('components/sidemenus.php');
+include('components/main.php');
+include('components/form.php');
+include('components/table.php');
+include('model/newsInsertion.php');
+echo $head.$menus.$mainHead.$row;
