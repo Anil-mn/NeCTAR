5149fb216cc683513ab65cf818840d353a794c69
ANIL MN <amn.nmb@gmail.com>
2020-04-24 13:09:10 +0530

    slider updated

--- Components/imageSlider.php
+++ Components/imageSlider.php
@@ -26,8 +26,10 @@ $Slider = '<!-- Slider 1 Area Start Here -->
                    <div class="slider-content s-tb slide-2">
                        <div class="title-container s-tb-c">
                            <div class="title1">'.$titles[1].'</div>
-                           <p>Emply dummy text of the printing and typesetting industry orem Ipsum has been the industrys standard
-                               <br>dummy text ever sinceprinting and typesetting industry. </p>
+                           <p>is a virtual conference.. an online event that includes a series of large sessions
+                           run by thought leaders in the industry who present to and interact with attendees
+                               <br>This would
+                               replicate a traditional conference in a virtual environment. </p>
                            <div class="slider-btn-area">
                                <a href="#" class="default-big-btn">Start a Course</a>
                            </div>
@@ -94,29 +96,27 @@ $slider2 ='<!-- Slider 1 Area Start Here -->
         <div id="ensign-nivoslider-3" class="slides">
         <img src="img/slider/ncerc1.jpg" alt="slider" title="#slider-direction-1" />
         <img src="img/slider/1-2.jpg" alt="slider" title="#slider-direction-2" />
-        <img src="img/slider/1-3.jpg" alt="slider" title="#slider-direction-3" />
+        <img src="img/slider/speaker.jpg" alt="slider" title="#slider-direction-3" />
     </div>
         <div id="slider-direction-1" class="t-cn slider-direction">
             <div class="slider-content s-tb slide-1">
-                <div class="title-container s-tb-c">
-                    <<div class="title1">'.$titles[1].'</div>
-                    <p>Emply dummy text of the printing and typesetting industry orem Ipsum has been the industrys standard
-                        <br>dummy text ever sinceprinting and typesetting industry. </p>
-                    <div class="slider-btn-area">
-                        <a href="#" class="default-big-btn">Start a Course</a>
-                    </div>
-                </div>
+            <div class="title-container s-tb-c">
+            <div class="title1">'.$titles[0].'</div>
+            <p>is a virtual conference.. an online event that includes a series of large sessions
+            run by thought leaders in the industry who present to and interact with attendees
+                <br>This would
+                replicate a traditional conference in a virtual environment. </p>
+            <div class="slider-btn-area">
+            </div>
             </div>
         </div>
         <div id="slider-direction-2" class="t-cn slider-direction">
             <div class="slider-content s-tb slide-2">
                 <div class="title-container s-tb-c">
                 <div class="title1">'.$titles[1].'</div>
-                <p>Emply dummy text of the printing and typesetting industry orem Ipsum has been the industrys standard
-                    <br>dummy text ever sinceprinting and typesetting industry. </p>
-                <div class="slider-btn-area">
-                        <a href="#" class="default-big-btn">Start a Course</a>
-                    </div>
+                <p>is organizing a fully online e-conference on Technologies AnnexingReality.
+                    <br>Technological annexations have provided our world with new environments and keeps the human horizons to grow. </p>
+                
                 </div>
             </div>
         </div>
@@ -124,11 +124,11 @@ $slider2 ='<!-- Slider 1 Area Start Here -->
             <div class="slider-content s-tb slide-3">
                 <div class="title-container s-tb-c">
                 <div class="title1">'.$titles[2].'</div>
-                <p>Emply dummy text of the printing and typesetting industry orem Ipsum has been the industrys standard
-                    <br>dummy text ever sinceprinting and typesetting industry. </p>
-                <div class="slider-btn-area">
-                        <a href="#" class="default-big-btn">Start a Course</a>
-                    </div>
+                <p>Technology makes our lives easier than what It was. It has made our life much faster. We don’t
+                have to wait for a long time to send and receive letters through the postal service
+                    <br> Travel is
+                    much faster than ever. </p>
+                
                 </div>
             </div>
         </div>
